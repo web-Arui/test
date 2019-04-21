8e5ae4e8dd35e679f389c4f7904afa617b70f025
web-Arui <48850002+web-Arui@users.noreply.github.com>
2019-04-21 22:28:32 +0800

    Update vue1.vue
    簡單的計算器

--- vue1.vue
+++ vue1.vue
@@ -1,1 +1,57 @@
-1111111
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="vue.js"></script>
+</head>
+
+<body>
+    <div id="app">
+        <input type="text" v-model="n1">
+        <select v-model="opt">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+        <input type="text" v-model="n2">
+        <input type="button" value="=" @click="ret">
+        <input type="text" v-model="end">
+    </div>
+    <script>
+        var vm = new Vue({
+            el: '#app',
+            data: {
+                n1: 0,
+                n2: 0,
+                end: 0,
+                opt: '+'
+            },
+            methods: {
+                ret() {
+                  
+                    switch (this.opt) {
+                        case '+':
+                            this.end = parseInt(this.n1) + parseInt(this.n2)
+                            break;
+                        case '-':
+                            this.end = parseInt(this.n1) - parseInt(this.n2)
+                            break;
+                        case '*':
+                            this.end = parseInt(this.n1) * parseInt(this.n2)
+                            break;
+                        case '/':
+                            this.end = parseInt(this.n1) / parseInt(this.n2)
+                            break;
+                    }
+                }
+            }
+        })
+    </script>
+</body>
+
+</html>
